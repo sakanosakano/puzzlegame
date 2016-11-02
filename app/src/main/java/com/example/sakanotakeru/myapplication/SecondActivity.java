9e89f9f15ab4303bb2342c26dd8dac7995c92ac3
坂野 <take.sakano@gmail.com>
2016-11-02 14:57:14 +0900

    timer途中

--- app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
@@ -3,9 +3,13 @@ package com.example.sakanotakeru.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -13,6 +17,9 @@ import android.widget.Button;
  */
 
 public class SecondActivity extends AppCompatActivity {
+    private Timer timer;
+    private CountUpTimerTask timerTask = null;
+    private Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,28 @@ public class SecondActivity extends AppCompatActivity {
                 finish();
             }
         });
+        timerStart();
+    }
+    public static void timerStart() {
+        // Timer インスタンスを生成
+        timer = new Timer();
+
+        // TimerTask インスタンスを生成
+        timerTask = new CountUpTimerTask();
+
+        // スケジュールを設定 100msec
+        // public void schedule (TimerTask task, long delay, long period)
+        timer.schedule(timerTask, 0, 100);
+    }
+    class CountUpTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // handlerを使って処理をキューイングする
+            handler.post(new Runnable() {
+                public void run() {
+
+                }
+            });
+        }
     }
 }
