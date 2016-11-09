186a77ccef00e1a948b5d7b0e3769a89bf48f961
坂野 <take.sakano@gmail.com>
2016-11-09 14:24:21 +0900

    画像切り替え追加。

--- app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,24 +17,33 @@ import java.util.TimerTask;
  * Created by sakano on 2016/10/26.
  */
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
     private Timer timer;
     private CountUpTimerTask timerTask = null;
     private Handler handler = new Handler();
+    private ImageView[] imageViews = new ImageView[9];
+    private static int[] resouces = {R.mipmap.droid1,R.mipmap.droid2,R.mipmap.droid3};
+    private static int count = 0;
+    Button endButton;
+    Button backButton;
+    Button suffleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Button backButton = (Button) findViewById(R.id.button2);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        imageViews = new ImageView[]{(ImageView) findViewById(R.id.panel0), (ImageView) findViewById(R.id.panel1),
+                (ImageView) findViewById(R.id.panel2), (ImageView) findViewById(R.id.panel3), (ImageView) findViewById(R.id.panel4),
+                (ImageView) findViewById(R.id.panel5), (ImageView) findViewById(R.id.panel6), (ImageView) findViewById(R.id.panel7),
+                (ImageView) findViewById(R.id.panel8)};
+        endButton = (Button) findViewById(R.id.endButton);
+        backButton = (Button) findViewById(R.id.stopButton);
+        suffleButton = (Button) findViewById(R.id.suffuleButton);
+        endButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
+        suffleButton.setOnClickListener(this);
         timerStart();
     }
-    public static void timerStart() {
+    public void timerStart() {
         // Timer インスタンスを生成
         timer = new Timer();
 
@@ -42,17 +52,33 @@ public class SecondActivity extends AppCompatActivity {
 
         // スケジュールを設定 100msec
         // public void schedule (TimerTask task, long delay, long period)
-        timer.schedule(timerTask, 0, 100);
+        timer.schedule(timerTask, 0, 200);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == endButton){
+            finish();
+        }else if(v == backButton){
+            timer.cancel();
+        }else if(v == suffleButton){
+            timerStart();
+        }
+    }
+
     class CountUpTimerTask extends TimerTask {
         @Override
         public void run() {
             // handlerを使って処理をキューイングする
             handler.post(new Runnable() {
                 public void run() {
-
+                    imageViews[0].setImageResource(resouces[count]);
                 }
             });
+            count++;
+            if (count > 2){
+                count = 0;
+            }
         }
     }
 }
