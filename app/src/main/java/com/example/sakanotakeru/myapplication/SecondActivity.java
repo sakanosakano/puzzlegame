559e68e73c0be7f15584c0975e19e0a534a009ed
坂野 <take.sakano@gmail.com>
2016-12-21 14:01:35 +0900

    必要ない行を削除。

--- app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/sakanotakeru/myapplication/SecondActivity.java
@@ -1,19 +1,15 @@
 package com.example.sakanotakeru.myapplication;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 
 /**
