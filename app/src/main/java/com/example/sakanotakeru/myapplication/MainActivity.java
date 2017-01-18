56ab0f879dfeaa04fa1cf9e58f7d161ad8add891
坂野 <take.sakano@gmail.com>
2017-01-18 14:16:20 +0900

    タイトル画面の編集

--- app/src/main/java/com/example/sakanotakeru/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sakanotakeru/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button nextButton = (Button) findViewById(R.id.botton1);
+        ImageButton nextButton = (ImageButton) findViewById(R.id.botton1);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
